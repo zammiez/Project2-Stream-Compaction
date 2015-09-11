4c6fb5d7d82b4fe4a178a15b1e294b9d0fb2947f
ZIWEI <GITHUB_USER@users.noreply.github.com>
2015-09-10 23:22:06 -0400

    Part1:CPU

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include "cpu.h"
-
+#include "common.h"
 namespace StreamCompaction {
 namespace CPU {
 
@@ -8,8 +8,17 @@ namespace CPU {
  * CPU scan (prefix sum).
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+	for (int i = 0; i < n; i++)
+	{
+		if (i==0)
+		{
+			odata[i] = 0;
+			continue;
+		}
+		odata[i] = odata[i - 1] + idata[i - 1];
+	}
+	// TODO
+    printf("StreamCompaction::CPU::scan : exclusive prefix sum.\n");
 }
 
 /**
@@ -18,8 +27,16 @@ void scan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithoutScan(int n, int *odata, const int *idata) {
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (idata[i] != 0)
+		{
+			odata[k++] = idata[i];
+		}
+	}
     // TODO
-    return -1;
+    return k;
 }
 
 /**
@@ -29,7 +46,35 @@ int compactWithoutScan(int n, int *odata, const int *idata) {
  */
 int compactWithScan(int n, int *odata, const int *idata) {
     // TODO
-    return -1;
+	int*TempArray = new int[n+1];
+	int*ScanArray = new int[n+1];
+	for (int i = 0; i < n; i++)
+	{
+		if (idata[i] != 0)
+		{
+			TempArray[i] = 1;
+		}
+		else
+		{
+			TempArray[i] = 0;
+		}
+	}
+	TempArray[n] = 0;
+	scan(n+1, ScanArray, TempArray);
+
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (TempArray[i]==1)
+		{
+			odata[ScanArray[i]] =  idata[i];
+			k++;
+		}
+	}
+	int count =  ScanArray[n];
+	delete[] TempArray;
+	delete[] ScanArray;
+	return count;
 }
 
 }
