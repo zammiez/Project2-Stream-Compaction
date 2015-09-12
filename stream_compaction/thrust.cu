a49c6caed112f8c799944cee22205b56d77be9c9
ZIWEI <GITHUB_USER@users.noreply.github.com>
2015-09-12 12:07:38 -0400

    Part 4 : thrust

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -16,6 +16,21 @@ void scan(int n, int *odata, const int *idata) {
     // TODO use `thrust::exclusive_scan`
     // example: for device_vectors dv_in and dv_out:
     // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+	
+
+	//thrust::host_vector<int> hst_in(n);
+	//hst_in.resize(n);
+	thrust::device_vector<int> dv_in (idata,idata+n) ;
+	thrust::device_vector<int> dv_out = dv_in;
+
+
+	//thrust::copy_n(idata, n, dv_in);
+
+	thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+	//thrust::exclusive_scan(idata, idata + n, odata);
+
+	thrust::copy(dv_out.begin(), dv_out.end(), odata);
+
 }
 
 }
